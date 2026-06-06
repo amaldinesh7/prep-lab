2506276c41f5930a652111cf74b8ca2ae0d3712c
amaldinesh7 <amal.dinesh@postman.com>
2026-06-06 11:28:01 +0530

    feat(content): register all modules + reseed DB

--- packages/content/src/manifest.ts
+++ packages/content/src/manifest.ts
@@ -1,4 +1,26 @@
 import type { ModuleDef } from "./types";
 import { tanstackRouterModule } from "./modules/tanstack-router/manifest";
+import { performanceModule } from "./modules/performance/manifest";
+import { optimisticUpdatesModule } from "./modules/optimistic-updates/manifest";
+import { serviceWorkersModule } from "./modules/service-workers/manifest";
+import { syncEnginesModule } from "./modules/sync-engines/manifest";
+import { crdtsModule } from "./modules/crdts/manifest";
+import { otModule } from "./modules/ot/manifest";
+import { webrtcModule } from "./modules/webrtc/manifest";
+import { reactNativeModule } from "./modules/react-native/manifest";
+import { electronModule } from "./modules/electron/manifest";
+import { capstoneModule } from "./modules/capstone/manifest";
 
-export const allModules: ModuleDef[] = [tanstackRouterModule];
+export const allModules: ModuleDef[] = [
+  tanstackRouterModule,
+  performanceModule,
+  optimisticUpdatesModule,
+  serviceWorkersModule,
+  syncEnginesModule,
+  crdtsModule,
+  otModule,
+  webrtcModule,
+  reactNativeModule,
+  electronModule,
+  capstoneModule,
+];
